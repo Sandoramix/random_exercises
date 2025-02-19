ffc91a3a3e55ebf5cf9884d7070b3f76ad5052ff
Oleksandr Dudniak <odudniak@student.42firenze.it>
2025-02-19 14:19:52 +0100

    57

---
+++ decode_nested_string.py.py
@@ -0,0 +1,12 @@
+"""
+Interviewer:
+Write a program to give the following output for the given input
+
+Example 1:
+Input: "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+Input: "3[a2[c]]"
+Output: "accaccacc"
+"""
